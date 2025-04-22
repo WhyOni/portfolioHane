cf52e89c5a707a30699248f2c3c6647d9b81c7c6
鬼神•鬼壽 I世 <amraa@amraas-MacBook-Pro.local>
2025-04-22 21:45:16 +0800

    plant model

--- portfoliahane/src/main.js
+++ portfoliahane/src/main.js
@@ -34,7 +34,6 @@ directionalLight.position.set(0, 3, 6);
 scene.add(directionalLight);
 
 const loader = new GLTFLoader();
-let hane = null;
 
 const composer = new EffectComposer(renderer);
 
@@ -51,44 +50,38 @@ composer.setSize(window.innerWidth, window.innerHeight);
 
 scene.background = new THREE.Color(0xbdbdbd);
 
+let hane = null,
+  clock,
+  mixer,
+  wave,
+  think,
+  lebron;
+
 loader.load(
   "HANE.gltf",
   function (han) {
     hane = han;
-    scene.add(han.scene);
-    han.scene.position.y = -0.7;
-    han.scene.rotation.y = (3 * Math.PI) / 4;
-    han.scene.position.x = 1;
-    han.scene.position.z = 1;
-
-    const clock = new THREE.Clock();
-    const mixer = new THREE.AnimationMixer(han.scene);
-    var wave = mixer.clipAction(han.animations[0]);
+    clock = new THREE.Clock();
+    mixer = new THREE.AnimationMixer(hane.scene);
+    wave = mixer.clipAction(hane.animations[0]);
     wave.setLoop(THREE.LoopOnce);
     wave.clampWhenFinished = true;
+    think = mixer.clipAction(hane.animations[1]);
+    lebron = mixer.clipAction(hane.animations[2]);
+    if (window.innerWidth / window.innerHeight >= 85 / 70) {
+      hane.scene.rotation.y = (3 * Math.PI) / 4;
+      hane.scene.position.x = 1;
+      hane.scene.position.y = -0.7;
+      hane.scene.position.z = 1;
+    } else {
+      hane.scene.rotation.y = (7 * Math.PI) / 8;
+      hane.scene.position.x = 0.5;
+      hane.scene.position.y = -0.7;
+      hane.scene.position.z = 0.7;
+    }
     wave.play();
 
     scene.add(han.scene);
-
-    function animate() {
-      const delta = clock.getDelta();
-      if (mixer) mixer.update(delta);
-      renderer.render(scene, camera);
-      composer.render();
-      requestAnimationFrame(animate);
-    }
-    animate();
-
-    document.getElementById("wavebutton").onclick = () => {
-      wave.stop();
-      wave.play();
-    };
-
-    function moveCamera() {
-      const t = document.body.getBoundingClientRect().top;
-    }
-
-    document.body.onscroll = moveCamera;
   },
   undefined,
   function (error) {
@@ -96,12 +89,109 @@ loader.load(
   }
 );
 
-// function animate() {
-//   //   if (hane !== null) {
-//   //     hane.scene.rotation.y += 0.01;
-//   //   }
-//   renderer.render(scene, camera);
-//   composer.render();
-// }
+let flower = null;
 
-// renderer.setAnimationLoop(animate);
+loader.load(
+  "flower.gltf",
+  function (han) {
+    flower = han;
+    han.scene.position.z = 3;
+    scene.add(han.scene);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+
+function animate() {
+  if (hane != null) {
+    const delta = clock.getDelta();
+    if (mixer) mixer.update(delta);
+    flower.scene.rotation.y += 0.01;
+  }
+  renderer.render(scene, camera);
+  composer.render();
+  requestAnimationFrame(animate);
+}
+animate();
+
+document.getElementById("wavebutton").onclick = () => {
+  wave.stop();
+  wave.play();
+};
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  if (hane != null) {
+    if (t >= -window.innerHeight / 2) {
+      if (window.innerWidth / window.innerHeight >= 85 / 70) {
+        hane.scene.rotation.x = 0;
+        hane.scene.rotation.y = (3 * Math.PI) / 4;
+        hane.scene.rotation.z = 0;
+        hane.scene.position.x = 1;
+        hane.scene.position.y = -0.7;
+        hane.scene.position.z = 1;
+      } else {
+        hane.scene.rotation.x = 0;
+        hane.scene.rotation.y = (7 * Math.PI) / 8;
+        hane.scene.rotation.z = 0;
+        hane.scene.position.x = 0.5;
+        hane.scene.position.y = -0.7;
+        hane.scene.position.z = 0.7;
+      }
+      think.stop();
+      wave.play();
+    } else if (t >= (-3 * window.innerHeight) / 2) {
+      if (window.innerWidth / window.innerHeight >= 85 / 70) {
+        hane.scene.rotation.x = 0;
+        hane.scene.rotation.y = (3 * Math.PI) / 4;
+        hane.scene.rotation.z = 0;
+        hane.scene.position.x = 0.7;
+        hane.scene.position.y = -0.7;
+        hane.scene.position.z = 1;
+      } else {
+        hane.scene.rotation.x = 0;
+        hane.scene.rotation.y = (7 * Math.PI) / 8;
+        hane.scene.rotation.z = 0;
+        hane.scene.position.x = 0;
+        hane.scene.position.y = -0.7;
+        hane.scene.position.z = 1;
+      }
+      wave.stop();
+      think.play();
+    } else if (t >= (-9 * window.innerHeight) / 2) {
+      hane.scene.rotation.x += 0.03;
+      hane.scene.rotation.y += 0.03;
+      hane.scene.rotation.z = 0;
+      hane.scene.position.x = 0;
+      hane.scene.position.y = 0;
+      hane.scene.position.z = 0;
+      think.stop();
+      lebron.stop();
+      console.log("a");
+    } else if (t >= (-11 * window.innerHeight) / 2) {
+      hane.scene.rotation.x = 0;
+      hane.scene.rotation.y = Math.PI;
+      hane.scene.rotation.z = 0;
+      hane.scene.position.x = 0;
+      hane.scene.position.y = -0.5;
+      hane.scene.position.z = 0;
+      flower.scene.position.z = 3;
+      flower.scene.position.y = 0;
+      lebron.play();
+    } else {
+      hane.scene.rotation.x = 0;
+      hane.scene.rotation.y = Math.PI;
+      hane.scene.rotation.z = Math.PI / 2;
+      hane.scene.position.x = 0;
+      hane.scene.position.y = 0.5;
+      hane.scene.position.z = -1;
+      flower.scene.position.z = 0;
+      flower.scene.position.y = -0.8;
+      lebron.play();
+    }
+  }
+}
+
+document.body.onscroll = moveCamera;
